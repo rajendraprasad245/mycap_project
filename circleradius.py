cc81e6700c255195b834204519e21cb0d57124b1
C.RAJENDRA PRASAD <85057692+rajendraprasad245@users.noreply.github.com>
2022-04-09 17:25:31 +0530

    created..
    it prints output..

---
+++ circleradius.py
@@ -0,0 +1,11 @@
+# import math module
+from math import pi
+ 
+# take input from user
+r = float(input ("Input the radius of the circle : "))
+ 
+# compute the area from radius of a circle given by user
+calculateArea = str(pi * r**2);
+ 
+#print result
+print ("The area of the circle with radius " + str(r) + " is: " + calculateArea)
